bdc573e29fe0828f54058efb75e9d4a33067bc96
shrutisingh2176 <singhshruti2029@gmail.com>
2025-10-12 21:59:25 +0530

    Implemented OperationStack

--- src/main/java/com/wagmi/finance/history/OperationStack.java
+++ src/main/java/com/wagmi/finance/history/OperationStack.java
@@ -13,22 +13,37 @@ public class OperationStack {
     private int top = -1;
 
     public OperationStack(int capacity) {
+        if ( capacity ==-1) { throw new UnsupportedOperationException("capacity should be positive");}
+
         this.data = new String[capacity];
     }
 
     public void push(String operation) {
         // stub
-        throw new UnsupportedOperationException("Not implemented");
+        if ( operation ==null) {
+            throw new UnsupportedOperationException("cannot push null value");
+        }
+        if ( top>= data.length-1) {
+            throw new UnsupportedOperationException("Stack Overflow");
+        }  else
+        { data[++top] = operation;}
     }
 
     public String pop() {
         // stub
-        throw new UnsupportedOperationException("Not implemented");
+        if (top==-1){
+        throw new UnsupportedOperationException("Stack Underflow");}
+        String value = data[top];
+        data [top--] = null;
+        return value;
     }
 
     public String peek() {
         // stub
-        throw new UnsupportedOperationException("Not implemented");
+        if (top==-1){
+        throw new UnsupportedOperationException("Stack is empty");
+    }
+        return data[top];
     }
 
     public boolean isEmpty() {
